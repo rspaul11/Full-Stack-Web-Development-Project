b1645e9feba7c7c85dd75e26ce145987f0b649a4
rspaul11 <rspaul1104@gmail.com>
2025-06-21 23:30:24 +0530

    Adding source code to files

--- Online-Banking-Full-Stack-Web-Application/AdminPortal/e2e/src/app.po.ts
+++ Online-Banking-Full-Stack-Web-Application/AdminPortal/e2e/src/app.po.ts
@@ -1,1 +1,11 @@
-app.po.ts
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}

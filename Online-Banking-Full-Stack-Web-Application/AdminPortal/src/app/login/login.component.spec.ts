51f1bffdad8c3b0111539be3c30866a2fa4d4f89
rspaul11 <rspaul1104@gmail.com>
2025-06-23 21:02:50 +0530

    Adding source code to files

--- Online-Banking-Full-Stack-Web-Application/AdminPortal/src/app/login/login.component.spec.ts
+++ Online-Banking-Full-Stack-Web-Application/AdminPortal/src/app/login/login.component.spec.ts
@@ -1,1 +1,25 @@
-login.component.spec.ts
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

c6b3e3431f3ee876f285747c35c9ef11ca0d3aa0
rspaul11 <rspaul1104@gmail.com>
2025-06-26 22:42:51 +0530

    Adding source code to files

--- Online-Banking-Full-Stack-Web-Application/AdminPortal/src/app/primary-transaction/primary-transaction.component.spec.ts
+++ Online-Banking-Full-Stack-Web-Application/AdminPortal/src/app/primary-transaction/primary-transaction.component.spec.ts
@@ -1,1 +1,25 @@
-primary-transaction.component.spec.ts
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrimaryTransactionComponent } from './primary-transaction.component';
+
+describe('PrimaryTransactionComponent', () => {
+  let component: PrimaryTransactionComponent;
+  let fixture: ComponentFixture<PrimaryTransactionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PrimaryTransactionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrimaryTransactionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

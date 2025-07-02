eb03f93c9d70e8900287c101e44aac9b65d3ff11
rspaul11 <rspaul1104@gmail.com>
2025-07-02 21:58:08 +0530

    Adding source code to files

--- Online-Banking-Full-Stack-Web-Application/UserFront/src/main/java/com/userfront/domain/security/Authority.java
+++ Online-Banking-Full-Stack-Web-Application/UserFront/src/main/java/com/userfront/domain/security/Authority.java
@@ -1,1 +1,20 @@
-Authority.java
+package com.userfront.domain.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ * Created by MTouiti on 11/05/18.
+ */
+public class Authority implements GrantedAuthority{
+
+    private final String authority;
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}

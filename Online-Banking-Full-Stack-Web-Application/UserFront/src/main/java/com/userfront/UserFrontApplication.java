82707c1522a1a64fef91ca52f634058af1775be1
rspaul11 <rspaul1104@gmail.com>
2025-07-04 21:45:49 +0530

    Adding source code to files

--- Online-Banking-Full-Stack-Web-Application/UserFront/src/main/java/com/userfront/UserFrontApplication.java
+++ Online-Banking-Full-Stack-Web-Application/UserFront/src/main/java/com/userfront/UserFrontApplication.java
@@ -1,1 +1,12 @@
-UserFrontApplication.java
+package com.userfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFrontApplication.class, args);
+	}
+}

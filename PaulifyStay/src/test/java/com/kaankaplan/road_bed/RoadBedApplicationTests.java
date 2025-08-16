52b5dfced6e774fa4a4efbc90bef1a7df5d4b18c
rspaul11 <rspaul1104@gmail.com>
2025-08-16 21:00:08 +0530

    Adding source code to file

--- PaulifyStay/src/test/java/com/kaankaplan/road_bed/RoadBedApplicationTests.java
+++ PaulifyStay/src/test/java/com/kaankaplan/road_bed/RoadBedApplicationTests.java
@@ -1,1 +1,13 @@
-RoadBedApplicationTests.java
+package com.kaankaplan.road_bed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadBedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

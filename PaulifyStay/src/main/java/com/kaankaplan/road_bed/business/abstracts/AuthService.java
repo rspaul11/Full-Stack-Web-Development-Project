946f7faeaf0bbe8c6c85c0de61bd533848534351
rspaul11 <rspaul1104@gmail.com>
2025-08-21 22:30:31 +0530

    Adding source code to file

--- PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/AuthService.java
+++ PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/AuthService.java
@@ -1,1 +1,14 @@
-AuthService.java
+package com.kaankaplan.road_bed.business.abstracts;
+
+import com.kaankaplan.road_bed.dtos.LoginRequest;
+import com.kaankaplan.road_bed.dtos.LoginResponse;
+import com.kaankaplan.road_bed.dtos.RefreshRequest;
+
+public interface AuthService {
+
+    LoginResponse login(LoginRequest loginRequest);
+
+    void logout(RefreshRequest refreshRequest);
+
+    LoginResponse refreshToken(RefreshRequest refreshRequest);
+}
--- PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/CategoryService.java
+++ PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/CategoryService.java
@@ -1,1 +1,13 @@
-CategoryService.java
+package com.kaankaplan.road_bed.business.abstracts;
+
+import com.kaankaplan.road_bed.entities.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<Category> getAllCategories();
+
+    Category save(Category category);
+
+}

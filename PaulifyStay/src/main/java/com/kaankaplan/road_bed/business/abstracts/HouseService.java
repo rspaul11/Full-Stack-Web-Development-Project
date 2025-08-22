52a0a2991b991e6483ed261123eeaefff40e4f70
rspaul11 <rspaul1104@gmail.com>
2025-08-22 22:41:52 +0530

    Adding source code to file

--- PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/HouseService.java
+++ PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/HouseService.java
@@ -1,1 +1,27 @@
-HouseService.java
+package com.kaankaplan.road_bed.business.abstracts;
+
+import com.kaankaplan.road_bed.dtos.ReserveHouseRequest;
+import com.kaankaplan.road_bed.entities.House;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+import java.util.List;
+
+public interface HouseService {
+
+    House save(House house, List<MultipartFile> multipartFiles);
+
+    List<House> getAll();
+
+    List<House> getHousesByCategoryName(String categoryName);
+
+    List<House> getHousesByCityName(String cityName);
+
+    House getHouseByHouseId(String houseId);
+
+    List<House> getHousesByCityAndEmptyDates(String cityName, Date startDate, Date endDate);
+
+    void reserveHouse(ReserveHouseRequest reserveHouseRequest);
+
+    House deleteHouse(House house);
+}

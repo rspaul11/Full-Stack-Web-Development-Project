8f3368242268bf8f9519eb7adc5d507de8f2fd73
rspaul11 <rspaul1104@gmail.com>
2025-08-24 22:02:57 +0530

    Adding source code to file

--- PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/RefreshTokenService.java
+++ PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/RefreshTokenService.java
@@ -1,1 +1,14 @@
-RefreshTokenService.java
+package com.kaankaplan.road_bed.business.abstracts;
+
+import com.kaankaplan.road_bed.entities.RefreshToken;
+
+
+public interface RefreshTokenService {
+
+    RefreshToken getRefreshTokenByToken(String refreshToken);
+
+    RefreshToken generateRefreshToken(String email);
+
+    void deleteRefreshToken(String refreshToken);
+
+}

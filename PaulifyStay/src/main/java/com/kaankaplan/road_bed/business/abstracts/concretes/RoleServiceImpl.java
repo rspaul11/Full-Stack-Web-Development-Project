f743705a0476b8b6d2928961b89693d029cda772
rspaul11 <rspaul1104@gmail.com>
2025-08-26 21:37:13 +0530

    Adding source code to file

--- PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/concretes/RoleServiceImpl.java
+++ PaulifyStay/src/main/java/com/kaankaplan/road_bed/business/abstracts/concretes/RoleServiceImpl.java
@@ -1,1 +1,32 @@
-RoleServiceImpl.java
+package com.kaankaplan.road_bed.business.abstracts.concretes;
+
+import com.kaankaplan.road_bed.business.abstracts.RoleService;
+import com.kaankaplan.road_bed.config.concerns.loging.ToLog;
+import com.kaankaplan.road_bed.entities.Role;
+import com.kaankaplan.road_bed.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role findByRoleName(String roleName) {
+        return roleRepository.findByRoleName(roleName);
+    }
+
+    @ToLog
+    @Transactional
+    @Override
+    public Role addRole(Role role) {
+        return roleRepository.insert(role);
+    }
+}

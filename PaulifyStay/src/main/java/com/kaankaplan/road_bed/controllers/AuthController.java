f1452b5688e1eb343c25b3455955e14e76e02b3b
rspaul11 <rspaul1104@gmail.com>
2025-08-29 22:33:03 +0530

    Adding source code to files

--- PaulifyStay/src/main/java/com/kaankaplan/road_bed/controllers/AuthController.java
+++ PaulifyStay/src/main/java/com/kaankaplan/road_bed/controllers/AuthController.java
@@ -1,1 +1,47 @@
-AuthController.java
+package com.kaankaplan.road_bed.controllers;
+
+import com.kaankaplan.road_bed.business.abstracts.AuthService;
+import com.kaankaplan.road_bed.business.abstracts.TenantService;
+import com.kaankaplan.road_bed.dtos.LoginRequest;
+import com.kaankaplan.road_bed.dtos.LoginResponse;
+import com.kaankaplan.road_bed.dtos.RefreshRequest;
+import com.kaankaplan.road_bed.dtos.TenantRegisterRequest;
+import com.kaankaplan.road_bed.entities.Tenant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth/")
+public class AuthController {
+
+    private final AuthService authService;
+    private final TenantService tenantService;
+
+    @Autowired
+    public AuthController(AuthService authService, TenantService tenantService) {
+        this.authService = authService;
+        this.tenantService = tenantService;
+    }
+
+    @PostMapping("register")
+    public Tenant register(@RequestBody TenantRegisterRequest tenantRegisterRequest) {
+        return tenantService.saveTenant(tenantRegisterRequest);
+    }
+
+    @PostMapping("login")
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+
+    @PostMapping("logout")
+    public String logout(@RequestBody RefreshRequest refreshRequest) {
+        authService.logout(refreshRequest);
+        return "LoggedOut";
+    }
+
+    @PostMapping("refresh/accessToken")
+    public LoginResponse refreshToken(@RequestBody RefreshRequest refreshRequest) {
+        return authService.refreshToken(refreshRequest);
+    }
+}AuthController.java
--- PaulifyStay/src/main/java/com/kaankaplan/road_bed/controllers/TenantController.java
+++ PaulifyStay/src/main/java/com/kaankaplan/road_bed/controllers/TenantController.java
@@ -1,1 +1,31 @@
-TenantController.java
+package com.kaankaplan.road_bed.controllers;
+
+import com.kaankaplan.road_bed.business.abstracts.TenantService;
+import com.kaankaplan.road_bed.entities.Tenant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("tenants/")
+public class TenantController {
+
+    private final TenantService tenantService;
+
+    @Autowired
+    public TenantController(TenantService tenantService) {
+        this.tenantService = tenantService;
+    }
+
+    @GetMapping("getByEmail/{email}")
+    public Tenant getByEmail(@PathVariable String email) {
+        return tenantService.getTenantByEmail(email);
+    }
+
+    @PreAuthorize("hasAnyRole('TENANT')")
+    @PostMapping("updateProfilePic/{userId}")
+    public Tenant updateProfilePicture(@PathVariable String userId, @RequestPart("picture") MultipartFile multipartFile) {
+        return tenantService.updateProfilePicture(userId, multipartFile);
+    }
+}

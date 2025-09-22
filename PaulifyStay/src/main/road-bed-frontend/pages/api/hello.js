949556b691b2a34af73d57cd48614cdf6a952840
rspaul11 <rspaul1104@gmail.com>
2025-09-22 22:33:35 +0530

    Adding source code to file

--- PaulifyStay/src/main/road-bed-frontend/pages/api/hello.js
+++ PaulifyStay/src/main/road-bed-frontend/pages/api/hello.js
@@ -1,1 +1,5 @@
-hello.js
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}

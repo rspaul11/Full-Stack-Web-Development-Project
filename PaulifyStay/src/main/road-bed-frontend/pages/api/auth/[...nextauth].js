2477af11d31884c2ca4bbba735ee916de6ca5d09
rspaul11 <rspaul1104@gmail.com>
2025-09-23 22:20:30 +0530

    Adding source code to file

--- PaulifyStay/src/main/road-bed-frontend/pages/api/auth/[...nextauth].js
+++ PaulifyStay/src/main/road-bed-frontend/pages/api/auth/[...nextauth].js
@@ -1,1 +1,61 @@
-[...nextauth].js
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+        
+        const result = await fetch("http://localhost:8080/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        });
+
+        const user = await result.json();
+
+        if (result.ok && user) return user;
+        else return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt"
+  },
+  callbacks:{
+    async jwt({token, user}) { 
+      if(user) {
+        token.userId = user.userId
+        token.accessToken = user.accessToken
+        token.refreshToken = user.refreshToken
+      }
+      return token;
+    },
+
+    async session({session, token}) {
+      if(token) {
+        session.userId = token.userId
+        session.accessToken = token.accessToken
+        session.refreshToken = token.refreshToken
+      }
+      return session;
+    }
+  },
+  pages: {
+    signIn: "/login"
+  }
+};
+
+export default NextAuth(authOptions);

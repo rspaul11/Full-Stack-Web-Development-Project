bcc4d3aa728236876d7a2a361e2b754c51fd1103
rspaul11 <rspaul1104@gmail.com>
2025-09-27 12:18:25 +0530

    Adding source code to the files

--- PaulWay/frontend/.eslintrc.js
+++ PaulWay/frontend/.eslintrc.js
@@ -1,1 +1,18 @@
-.eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {}
+}

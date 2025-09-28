1cec1a35193deb1ff2e0d10fca17de987cb8c341
rspaul11 <rspaul1104@gmail.com>
2025-09-28 21:14:44 +0530

    Adding source code to the files

--- PaulWay/frontend/nuxt.config.js
+++ PaulWay/frontend/nuxt.config.js
@@ -1,1 +1,104 @@
-nuxt.config.js
+nuxt.config.jsconst nodeExternals = require('webpack-node-externals')
+
+export default {
+  mode: "universal",
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    "~/plugins/services.js",
+    "~/plugins/interceptor.js",
+    "~/plugins/bootstrapvue.js",
+    "~/plugins/mapsplugin.js",
+    "~/plugins/composition-api-plugin.js",
+    "~/plugins/vue-carousel-card.js",
+    // { src: '~/plugins/vuetify-google-autocomplete.js', ssr: false},
+    { src: "~/plugins/slide-menu.js", ssr: false },
+    { src: '~/plugins/amcharts.js', ssr: false }
+  ],
+  // consider using this. autoimports components
+  // components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    "@nuxtjs/eslint-module",
+    '@nuxtjs/vuetify',
+    '@nuxtjs/fontawesome',
+  ],
+  //todo: limit only to the icons used
+  fontawesome: {
+    component: 'fa',
+    icons: {
+      solid: ['faHome', 'faUser', 'faMap', 'faGlobe', 'faCompass', 'faPowerOff'],
+      brands: ['faGoogle']
+    }
+  },
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    "@nuxtjs/axios"
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {
+    proxy: true,
+    host: "localhost",
+    prefix: "/api/"
+  },
+  proxy: {
+    "/api/": process.env.PROXY_API || "http://localhost:8080/"
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    transpile: [
+      'vue2-google-maps',
+      'vue-browser-geolocation',
+      // 'vuetify-google-autocomplete',
+      '@amcharts/amcharts4'
+    ],
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        const options = {
+          exclude: ['node_modules']
+        }
+        const EslintPlugin = require('eslint-webpack-plugin')
+        config.plugins.push(new EslintPlugin(options))
+      }
+    }
+  }
+};

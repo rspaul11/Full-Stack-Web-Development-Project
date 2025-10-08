a829b5450b6c0d17952b2c53fa5f6fd6c90663e5
rspaul11 <rspaul1104@gmail.com>
2025-10-08 22:45:29 +0530

    Adding files to the folder

---
+++ PaulWay/frontend/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button :style="{ background: color }" class="btn" @click="onclick()">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: 'black'
+    }
+  },
+  methods: {
+    onclick () {
+      this.$emit('btn-click')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+</style>

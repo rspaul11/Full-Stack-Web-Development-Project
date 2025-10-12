baca3c3902f168e23b33f88d56a7b68baf70068d
rspaul11 <rspaul1104@gmail.com>
2025-10-12 22:02:58 +0530

    Adding files to the folder

---
+++ PaulWay/frontend/components/SmallCard.vue
@@ -0,0 +1,71 @@
+<template>
+    <nuxt-link :to="`/cities/${city.name}`" class="card-container">
+      <div class="text-overlay" v-if="pictureUrl">
+        <img class="image" :src=pictureUrl alt="" v-tooltip.top-center="msg" />
+      </div>
+    </nuxt-link>
+</template>
+
+<script>
+import Vue from 'vue'
+import VTooltip from 'v-tooltip'
+Vue.use(VTooltip)
+
+    export default {
+        props: ["city"],
+      data(){
+        return {
+          pictureUrl: null,
+          msg: this.city.name
+        }
+      },
+      async created() {
+        if (this.pictureUrl == null) {
+          this.pictureUrl = await this.getPlacePicture()
+        }
+      },
+      methods: {
+        //todo: JSON.parse: unexpected character at line 1 column 1 of the JSON data
+      async getPlacePicture () {
+        var imageQuery = this.city.name.replace(/\s+/g, '-').replace(/,/g, '').toLowerCase();
+        const res = await fetch(`https://api.teleport.org/api/urban_areas/slug:${imageQuery}/images/`)
+        const data = await res.json()
+        var imageLink
+        if (data.photos){
+          imageLink = data.photos[0].image.mobile
+        } else {
+          var randomRes = await fetch('https://picsum.photos/300/200')
+          const randomData = await randomRes
+          imageLink = randomData.url
+        }
+        return imageLink
+      }
+      }
+    }
+</script>
+
+<style scoped>
+    .card-container {
+        width: 24%;
+        height: 12.5rem;
+        border-radius: 0.5rem;
+        overflow: hidden;
+        margin-bottom: 1.5rem;
+        cursor: pointer;
+    }
+
+    .text-overlay {
+      position: center;
+      height: 100%;
+      width: 100%;
+      transition: .5s ease;
+      background-color: rgb(255, 255, 255);
+    }
+    .text-overlay:hover {
+      opacity: 0.4;
+    }
+    .image {
+        width: 100%;
+        height: 100%
+    }
+</style>
---
+++ PaulWay/frontend/components/SmallCardDisplay.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <h3 class="header">{{ citiesList.title }}</h3>
+        <div class="card-container">
+            <SmallCard
+                v-for="city in getRandomPlaces(citiesList)"
+                :key="city.name"
+                :city="city"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import SmallCard from "~/components/SmallCard";
+
+export default {
+        props: ["citiesList"],
+      components: {
+        SmallCard
+      },
+      methods: {
+          getRandomPlaces(citiesList){
+            const n = 12;
+            return citiesList.cities
+              .map(x => ({ x, r: Math.random() }))
+              .sort((a, b) => a.r - b.r)
+              .map(a => a.x)
+              .slice(0, n);
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+    .container {
+      width: 80%;
+      min-height: 20vh;
+    }
+
+    .header {
+        padding-bottom: 15px;
+        font-weight: 700;
+        font-size: 1.5rem;
+    }
+
+    .card-container {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+</style>

7745bd8fd87d7ea018eabed4261d9ca40957c1a6
rspaul11 <rspaul1104@gmail.com>
2026-01-10 20:04:26 +0530

    Adding new folders and file

---
+++ Paulournal/src/test/java/com/karna/ankur/Journal/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karna.ankur.Journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

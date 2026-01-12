f17e65ab0955c530a3a98e0f73db4e1e2432b562
rspaul11 <rspaul1104@gmail.com>
2026-01-12 09:49:32 +0530

    Adding new file to the folder

---
+++ Paulournal/src/test/java/com/karna/ankur/Journal/Config/UserSchedulerTests.java
@@ -0,0 +1,17 @@
+//package com.karna.ankur.Journal.Config;
+//
+//import com.karna.ankur.Journal.Scheduler.UserScheduler;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class UserSchedulerTests {
+//
+//    @Autowired
+//    UserScheduler userScheduler;
+//    @Test
+//    public void testFetchUserAndSendSAMail(){
+//        userScheduler.fetchUsersAndSendSAMail();
+//    }
+//}

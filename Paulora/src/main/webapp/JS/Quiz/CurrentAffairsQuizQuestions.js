396330304aacd29304dd9d017d861930f2873196
rspaul11 <rspaul1104@gmail.com>
2026-02-14 22:10:34 +0530

    Adding new files to the folder

---
+++ Paulora/src/main/webapp/JS/Quiz/CurrentAffairsQuizQuestions.js
@@ -0,0 +1,153 @@
+
+/* Current Affairs Quiz Questions
+ * creating an array and passing the number, questions, options, and answers
+ */
+
+let questions = [
+
+/* Q1 */     
+	{
+      numb: 1,
+      question: "The employees of which MNC has recently formed a Union in the United States?",
+      options: [
+                "A) Microsoft",
+                "B) Tesla",
+                "C) Google",
+                "D) Apple"
+               ],
+      answer: "C) Google"
+    },
+/*Copy till here*/
+
+
+/* Q2 */     
+	{
+      numb: 2,
+      question: "Who is the head of the panel for conserving Ladakh's land and culture?",
+      options: [
+                "A) Amit Shah",
+                "B) G Kishan Reddy",
+                "C) Rao Inderjit Singh",
+                "D) Jitendra Singh"
+               ],
+      answer: "B) G Kishan Reddy"
+    },
+/*Copy till here*/
+
+/* Q3 */     
+	{
+      numb: 3,
+      question: "Kaziranga National Park is located along which Indian River?",
+      options: [
+                "A) Ganga",
+                "B) Yamuna",
+                "C) Brahmaputra",
+                "D) Sutlej"
+               ],
+      answer: "C) Brahmaputra"
+    },
+/*Copy till here*/
+	
+/* Q4 */     
+	{
+      numb: 4,
+      question: "Which bank acquired the Indian consumer banking businesses of Citibank?",
+      options: [
+                "A) HDFC Bank",
+                "B) Axis Bank",
+                "C) ICICI Bank",
+                "D) Yes Bank"
+               ],
+      answer: "B) Axis Bank"
+    },
+/*Copy till here*/
+	
+/* Q5 */     
+	{
+      numb: 5,
+      question: "The Lok Sabha passed a bill to merge three municipal corporations of which city into a single entity?",
+      options: [
+                "A) Mumbai",
+                "B) Delhi",
+                "C) Kolkata",
+                "D) Chennai"
+               ],
+      answer: "B) Delhi"
+    },
+/*Copy till here*/
+
+/* Q6 */     
+	{
+      numb: 6,
+      question: "Union Environment Ministry has designated which day as 'National Dolphin Day'?",
+      options: [
+                "A) April 5",
+                "B) May 5",
+                "C) July 5",
+                "D) October 5"
+               ],
+      answer: "D) October 5"
+    },
+/*Copy till here*/
+
+/* Q7 */     
+	{
+      numb: 7,
+      question: "Which Naval Ship was recently presented the prestigious President's Colour?",
+      options: [
+                "A) INS Valsura",
+                "B) INS Gomati",
+                "C) INS Brahmaputra",
+                "D) INS Betwa"
+               ],
+      answer: "A) INS Valsura"
+    },
+/*Copy till here*/
+
+/* Q8 */ 
+	{
+      numb: 8,
+      question: "Which global association released the 'Declaration on Privacy and the protection of personal data'?",
+      options: [
+                "A) G-20",
+                "B) European Union",
+                "C) G-7",
+                "D) NATO"
+               ],
+      answer: "B) European Union"
+    },
+/*Copy till here*/
+
+/* Q9 */ 
+	{
+      numb: 9,
+      question: "'Green Triangle' named after Mahatma Gandhi was inaugurated in which country?",
+      options: [
+                "A) Sri Lanka",
+                "B) Nepal",
+                "C) Madagascar",
+                "D) Myanmar"
+               ],
+      answer: "C) Madagascar"
+    },
+/*Copy till here*/
+
+
+/* Q10 */    
+	{
+      numb: 10,
+      question: "'Dishaank' is a land digitisation application of which Indian state?",
+      options: [
+                "A) Karnataka",
+                "B) Kerala",
+                "C) Andhra Pradesh",
+                "D) Gujarat"
+               ],
+      answer: "A) Karnataka"
+    },
+/*Copy till here*/
+
+
+
+
+]; /* Array close*/
---
+++ Paulora/src/main/webapp/JS/Quiz/EnglishQuizQuestions.js
@@ -0,0 +1,149 @@
+
+/* English Quiz Questions
+ * creating an array and passing the number, questions, options, and answers
+ */
+
+let questions = [
+
+/*Q1*/
+    {
+      numb: 1,
+      question: "Which is the best synonym for <br> 'BRIEF'.",
+      options: [
+                "A) Limited",
+                "B) Short",
+                "C) Little",
+                "D) Small"
+               ],
+      answer: "B) Short"
+    },
+
+/*Q2*/
+    {
+      numb: 2,
+      question: "Which is the best synonym for <br> 'CORPULENT'.",
+      options: [
+                "A) Lean",
+                "B) Gaunt",
+                "C) Emaciated",
+                "D) Obese"
+               ],
+      answer: "D) Obese"
+    },
+    
+/* Q No. */
+   {
+     numb: 3,
+     question: "Find the correctly spelt word",
+     options: [
+               "A) Efficient",
+               "B) Treatmeant",
+               "C) Beterment",
+               "D) Employd"
+              ],
+     answer: "A) Efficient"
+   },
+
+/* Q4 */     
+  {
+      numb: 4,
+      question: "Find the correctly spelt word",
+      options: [
+                "A) Ommineous",
+                "B) Omineous",
+                "C) Ominous",
+                "D) Omenous"
+               ],
+      answer: "C) Ominous"
+    },
+/*Copy till here*/
+  
+/* Q5 */     
+  {
+      numb: 5,
+      question: "Choose correct meaning of the idioms <br> 'To be above board'",
+      options: [
+                "A) To have a good height",
+                "B) To be honest in any business deal",
+                "C) They have no debts",
+                "D) To try to be beautiful"
+               ],
+      answer: "B) To be honest in any business deal"
+    },
+/*Copy till here*/
+
+/* Q6 */     
+  {
+      numb: 6,
+      question: "Choose correct meaning of the idioms <br> 'To put one's hand to plough'",
+      options: [
+                "A) To take up agricultural farming",
+                "B) To get entangled into unnecessary things",
+                "C) Take interest in technical work",
+                "D) To take a difficult tas"
+               ],
+      answer: "D) To take a difficult tas"
+    },
+/*Copy till here*/
+
+/* Q7 */     
+  {
+      numb: 7,
+      question: "Complete the sentence <br> I saw a ...... of cows in the field.",
+      options: [
+                "A) group",
+                "B) herd",
+                "C) swarm",
+                "D) flock"
+               ],
+      answer: "B) herd"
+    },
+/*Copy till here*/
+
+/* Q8 */ 
+  {
+      numb: 8,
+      question: "Complete the sentence <br> The miser gazed ...... at the pile of gold coins in front of him.",
+      options: [
+                "A) avidly",
+                "B) admiringly",
+                "C) thoughtfully",
+                "D) earnestly"
+               ],
+      answer: "A) avidly"
+    },
+/*Copy till here*/
+
+/* Q9 */ 
+  {
+      numb: 9,
+      question: "Select the pair which has the same relationship. <br> DIVA:OPERA",
+      options: [
+                "A) producer:theatre",
+                "B) director:drama",
+                "C) conductor:bus",
+                "D) thespian:play"
+               ],
+      answer: "D) thespian:play"
+    },
+/*Copy till here*/
+
+
+/* Q10 */    
+  {
+      numb: 10,
+      question: "Select the pair which has the same relationship. <br> PAIN:SEDATIVE",
+      options: [
+                "A) comfort:stimulant",
+                "B) grief:consolation",
+                "C) trance:narcotic",
+                "D) ache:extraction"
+               ],
+      answer: "B) grief:consolation"
+    },
+/*Copy till here*/
+
+
+
+
+]; /* Array close*/

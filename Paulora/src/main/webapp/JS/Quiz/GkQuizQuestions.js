8345219828a043da80ae003196aadf97bc1e3c89
rspaul11 <rspaul1104@gmail.com>
2026-02-15 22:31:37 +0530

    Adding new files to the folder

---
+++ Paulora/src/main/webapp/JS/Quiz/GkQuizQuestions.js
@@ -0,0 +1,153 @@
+
+/* GK Quiz Questions
+ * creating an array and passing the number, questions, options, and answers
+ */
+
+let questions = [
+
+/* Q1 */     
+  {
+      numb: 1,
+      question: "National Income estimates in India are prepared by",
+      options: [
+                "A) Planning Commission",
+                "B) Reserve Bank of India",
+                "C) Central statistical organisation",
+                "D) Indian statistical Institute"
+               ],
+      answer: "C) Central statistical organisation"
+    },
+/*Copy till here*/
+
+
+/* Q2 */     
+  {
+      numb: 2,
+      question: "The tropic of cancer does not pass through which of these Indian states?",
+      options: [
+                "A) Madhya Pradesh",
+                "B) West Bengal",
+                "C) Rajasthan",
+                "D) Odisha"
+               ],
+      answer: "D) Odisha"
+    },
+/*Copy till here*/
+
+/* Q3 */     
+  {
+      numb: 3,
+      question: "A computer cannot 'boot' if it does not have the _____",
+      options: [
+                "A) Compiler",
+                "B) Loader",
+                "C) Operating system",
+                "D) Assembler"
+               ],
+      answer: "C) Operating system"
+    },
+/*Copy till here*/
+  
+/* Q4 */     
+  {
+      numb: 4,
+      question: "The purest form of iron is ",
+      options: [
+                "A) wrought iron",
+                "B) steel",
+                "C) pig iron",
+                "D) nickel steel"
+               ],
+      answer: "A) wrought iron"
+    },
+/*Copy till here*/
+  
+/* Q5 */     
+  {
+      numb: 5,
+      question: "The working principle of a washing machine is",
+      options: [
+                "A) Reverse osmosis",
+                "B) Diffusion",
+                "C) Centrifugation",
+                "D) Dialysis"
+               ],
+      answer: "C) Centrifugation"
+    },
+/*Copy till here*/
+
+/* Q6 */     
+  {
+      numb: 6,
+      question: "Which river of India is called Vridha Ganga?",
+      options: [
+                "A) Krishna",
+                "B) Godavari",
+                "C) Kaveri",
+                "D) Narmada"
+               ],
+      answer: "B) Godavari"
+    },
+/*Copy till here*/
+
+/* Q7 */     
+  {
+      numb: 7,
+      question: "________ is the process of dividing the disk into tracks and sectors",
+      options: [
+                "A) Tracking",
+                "B) Formatting",
+                "C) Crashing",
+                "D) Allotting"
+               ],
+      answer: "B) Formatting"
+    },
+/*Copy till here*/
+
+/* Q8 */ 
+  {
+      numb: 8,
+      question: "Epsom(England) is the place associated with",
+      options: [
+                "A) Snooker",
+                "B) Shooting",
+                "C) Polo",
+                "D) Horse racing"
+               ],
+      answer: "D) Horse racing"
+    },
+/*Copy till here*/
+
+/* Q9 */ 
+  {
+      numb: 9,
+      question: "Junk e-mail is also called ______",
+      options: [
+                "A) Spam",
+                "B) Spoof",
+                "C) Sniffer script",
+                "D) Spool"
+               ],
+      answer: "A) Spam"
+    },
+/*Copy till here*/
+
+
+/* Q10 */    
+  {
+      numb: 10,
+      question: "The blue colour of the clear sky is due to",
+      options: [
+                "A) Diffraction of light",
+                "B) Dispersion of light",
+                "C) Reflection of light",
+                "D) Refraction of light"
+               ],
+      answer: "B) Dispersion of light"
+    },
+/*Copy till here*/
+
+
+
+
+]; /* Array close*/
---
+++ Paulora/src/main/webapp/JS/Quiz/HtmlCssQuizQuestions.js
@@ -0,0 +1,151 @@
+
+/* HtmlCSS Quiz Questions 
+ * creating an array and passing the number, questions, options, and answers 
+ */
+
+let questions = [
+
+/* Q1 */     
+	{
+      numb: 1,
+      question: "CSS stands for -",
+      options: [
+                "A) Cascade style sheets",
+                "B) Color and style sheets",
+                "C) Cascading style sheets",
+                "D) None of the above"
+               ],
+      answer: "C) Cascading style sheets"
+    },
+/*Copy till here*/
+
+
+/* Q2 */     
+	{
+      numb: 2,
+      question: "The property in CSS used to change the background color of an element is -",
+      options: [
+                "A) bgcolor",
+                "B) color",
+                "C) background-color",
+                "D) All of the above"
+               ],
+      answer: "C) background-color"
+    },
+/*Copy till here*/
+
+/* Q3 */     
+	{
+      numb: 3,
+      question: "The CSS property used to control the element's font-size is -",
+      options: [
+                "A) text-style",
+                "B) text-size",
+                "C) font-size",
+                "D) None of the above"
+               ],
+      answer: "C) font-size"
+    },
+/*Copy till here*/
+	
+/* Q4 */     
+	{
+      numb: 4,
+      question: "Which of the following CSS property is used to set the background image of an element?",
+      options: [
+                "A) background-attachment",
+                "B) background-image",
+                "C) background-color",
+                "D) None of the above"
+               ],
+      answer: "B) background-image"
+    },
+/*Copy till here*/
+	
+/* Q5 */     
+	{
+      numb: 5,
+      question: "Are the negative values allowed in padding property?",
+      options: [
+                "A) Yes",
+                "B) No",
+                "C) Can't say",
+                "D) May be"
+               ],
+      answer: "B) No"
+    },
+/*Copy till here*/
+
+/* Q6 */     
+	{
+      numb: 6,
+      question: "Which of the following property is used as the shorthand property of margin properties?",
+      options: [
+                "A) margin-left",
+                "B) margin-right",
+                "C) margin",
+                "D) None of the above"
+               ],
+      answer: "C) margin"
+    },
+/*Copy till here*/
+
+/* Q7 */     
+	{
+      numb: 7,
+      question: "The CSS property used to specify the transparency of an element is -",
+      options: [
+                "A) opacity",
+                "B) filter",
+                "C) visibility",
+                "D) overlay"
+               ],
+      answer: "A) opacity"
+    },
+/*Copy till here*/
+
+/* Q8 */ 
+	{
+      numb: 8,
+      question: "How to select the elements with the class name 'example'?",
+      options: [
+                "A) .example",
+                "B) .#example",
+                "C) example",
+                "D) .Class example"
+               ],
+      answer: "A) .example"
+    },
+/*Copy till here*/
+
+/* Q9 */ 
+	{
+      numb: 9,
+      question: "Which of the following is the correct syntax to select the p siblings of a div element?",
+      options: [
+                "A) p",
+                "B) div + p",
+                "C) div p",
+                "D) div ~ p"
+               ],
+      answer: "D) div ~ p"
+    },
+/*Copy till here*/
+
+
+/* Q10 */    
+	{
+      numb: 10,
+      question: "The CSS property used to set the distance between the borders of the adjacent cells in the table is -",
+      options: [
+                "A) border-collapse",
+                "B) border-radius",
+                "C) border-spacing",
+                "D) None of the above"
+               ],
+      answer: "C) border-spacing"
+    },
+/*Copy till here*/
+
+
+]; /* Array close*/

e100f6c4a8e97b7600aef48ebb2b47b81a3b11e4
rspaul11 <rspaul1104@gmail.com>
2026-02-17 08:45:31 +0530

    Adding new files to the folder

---
+++ Paulora/src/main/webapp/JS/Quiz/JavaQuizQuestions.js
@@ -0,0 +1,152 @@
+
+/* JavaQuiz Questions
+ * creating an array and passing the number, questions, options, and answers
+ */
+
+let questions = [
+/* Q1 */     
+  {
+      numb: 1,
+      question: "Java is a ___________ programming language",
+      options: [
+                "A) sequence-dependent",
+                "B) code-dependent",
+                "C) platform-dependent",
+                "D) platform-independent"
+               ],
+      answer: "D) platform-independent"
+    },
+/*Copy till here*/
+
+
+/* Q2 */     
+  {
+      numb: 2,
+      question: "Which component is used to compile, debug and execute the java programs?",
+      options: [
+                "A) JRE",
+                "B) JIT",
+                "C) JDK",
+                "D) JVM"
+               ],
+      answer: "C) JDK"
+    },
+/*Copy till here*/
+
+/* Q3 */     
+  {
+      numb: 3,
+      question: "Which one of the following is not a Java feature?",
+      options: [
+                "A) Object-oriented",
+                "B) Use of pointers",
+                "C) Portable",
+                "D) Dynamic and Extensible"
+               ],
+      answer: "B) Use of pointers"
+    },
+/*Copy till here*/
+  
+/* Q4 */     
+  {
+      numb: 4,
+      question: "Which of these cannot be used for a variable name in Java?",
+      options: [
+                "A) identifier & keyword",
+                "B) identifier",
+                "C) keyword",
+                "D) none of the mentioned"
+               ],
+      answer: "C) keyword"
+    },
+/*Copy till here*/
+  
+/* Q5 */     
+  {
+      numb: 5,
+      question: "What is the extension of java code files?",
+      options: [
+                "A) .js",
+                "B) .txt",
+                "C) .class",
+                "D) .java"
+               ],
+      answer: "D) .java"
+    },
+/*Copy till here*/
+
+/* Q6 */     
+  {
+      numb: 6,
+      question: "Which environment variable is used to set the java path?",
+      options: [
+                "A) MAVEN_Path",
+                "B) JavaPATH",
+                "C) JAVA",
+                "D) JAVA_HOME"
+               ],
+      answer: "D) JAVA_HOME"
+    },
+/*Copy till here*/
+
+/* Q7 */     
+  {
+      numb: 7,
+      question: "Which of the following is not an OOPS concept in Java?",
+      options: [
+                "A) Polymorphism",
+                "B) Inheritance",
+                "C) Compilation",
+                "D) Encapsulation"
+               ],
+      answer: "C) Compilation"
+    },
+/*Copy till here*/
+
+/* Q8 */ 
+  {
+      numb: 8,
+      question: "Which of these keywords is used to define interfaces in Java?",
+      options: [
+                "A) intf",
+                "B) Intf",
+                "C) interface",
+                "D) Interface"
+               ],
+      answer: "C) interface"
+    },
+/*Copy till here*/
+
+/* Q9 */ 
+  {
+      numb: 9,
+      question: "What is not the use of <span style='color: violet'>this</span> keyword in Java?",
+      options: [
+                "A) Referring to the instance variable when a local variable has the same name",
+                "B) Passing itself to the method of the same class",
+                "C) Passing itself to another method",
+                "D) Calling another constructor in constructor chaining"
+               ],
+      answer: "B) Passing itself to the method of the same class"
+    },
+/*Copy till here*/
+
+
+/* Q10 */    
+  {
+      numb: 10,
+      question: "Which exception is thrown when java is out of memory?",
+      options: [
+                "A) MemoryError",
+                "B) OutOfMemoryError",
+                "C) MemoryOutOfBoundsException",
+                "D) MemoryFullException"
+               ],
+      answer: "B) OutOfMemoryError"
+    },
+/*Copy till here*/
+
+
+
+
+]; /* Array close*/
---
+++ Paulora/src/main/webapp/JS/Quiz/PhpQuizQuestions.js
@@ -0,0 +1,151 @@
+
+/*  PHP Quiz Questions
+ * creating an array and passing the number, questions, options, and answers 
+ */
+
+let questions = [
+
+/* Q1 */     
+	{
+      numb: 1,
+      question: "PHP stands for",
+      options: [
+                "A) Hypertext Preprocessor",
+                "B) Pretext Hypertext Preprocessor",
+                "C) Personal Home Processor",
+                "D) None of the above"
+               ],
+      answer: "A) Hypertext Preprocessor"
+    },
+/*Copy till here*/
+
+
+/* Q2 */     
+	{
+      numb: 2,
+      question: "Who is known as the father of PHP?",
+      options: [
+                "A) Drek Kolkevi",
+                "B) List Barely",
+                "C) Rasmus Lerdrof",
+                "D) None of the above"
+               ],
+      answer: "C) Rasmus Lerdrof"
+    },
+/*Copy till here*/
+
+/* Q3 */     
+	{
+      numb: 3,
+      question: "Variable name in PHP starts with -",
+      options: [
+                "A) ! (Exclamation)",
+                "B) $ (Dollar)",
+                "C) & (Ampersand)",
+                "D) # (Hash)"
+               ],
+      answer: "B) $ (Dollar)"
+    },
+/*Copy till here*/
+	
+/* Q4 */     
+	{
+      numb: 4,
+      question: "Which of the following is not a variable scope in PHP?",
+      options: [
+                "A) Extern",
+                "B) Local",
+                "C) Static",
+                "D) Global"
+               ],
+      answer: "A) Extern"
+    },
+/*Copy till here*/
+	
+/* Q5 */     
+	{
+      numb: 5,
+      question: "Which of the following is correct to add a comment in php?",
+      options: [
+                "A) & .... &",
+                "B) // ....",
+                "C) /* .... */",
+                "D) Both (b) and (c)"
+               ],
+      answer: "D) Both (b) and (c)"
+    },
+/*Copy till here*/
+
+/* Q6 */     
+	{
+      numb: 6,
+      question: " Which of the following is used to display the output in PHP?",
+      options: [
+                "A) echo",
+                "B) write",
+                "C) print",
+                "D) Both (a) and (c)"
+               ],
+      answer: "D) Both (a) and (c)"
+    },
+/*Copy till here*/
+
+/* Q7 */     
+	{
+      numb: 7,
+      question: "Which of the following is used for concatenation in PHP?",
+      options: [
+                "A) + (plus)",
+                "B) * (Asterisk)",
+                "C) . (dot)",
+                "D) append()"
+               ],
+      answer: "C) . (dot)"
+    },
+/*Copy till here*/
+
+/* Q8 */ 
+	{
+      numb: 8,
+      question: "Which of the following starts with __ (double underscore) in PHP?",
+      options: [
+                "A) Inbuilt constants",
+                "B) User-defined constants",
+                "C) Magic constants",
+                "D) Default constants"
+               ],
+      answer: "C) Magic constants"
+    },
+/*Copy till here*/
+
+/* Q9 */ 
+	{
+      numb: 9,
+      question: "Which of the following is the correct way of defining a variable in PHP?",
+      options: [
+                "A) $variable name = value",
+                "B) $variable_name = value;",
+                "C) $variable_name = value",
+                "D) $variable name as value"
+               ],
+      answer: "B) $variable_name = value;"
+    },
+/*Copy till here*/
+
+
+/* Q10 */    
+	{
+      numb: 10,
+      question: "Which of the following function displays the information about PHP and its configuration?",
+      options: [
+                "A) php_info()",
+                "B) phpinfo()",
+                "C) info()",
+                "D) None of the above"
+               ],
+      answer: "B) phpinfo()"
+    },
+/*Copy till here*/
+
+
+]; /* Array close*/

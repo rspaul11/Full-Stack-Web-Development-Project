2265f3f8e461c4e5f73ca6f71da9f0035d150ed7
rspaul11 <rspaul1104@gmail.com>
2026-02-18 08:55:50 +0530

    Adding new files to the folder

---
+++ Paulora/src/main/webapp/JS/Quiz/PythonQuizQuestions.js
@@ -0,0 +1,151 @@
+
+/* Python Quiz Questions
+ * creating an array and passing the number, questions, options, and answers 
+ */
+
+let questions = [
+
+/* Q1 */     
+	{
+      numb: 1,
+      question: "Who developed Python Programming Language?",
+      options: [
+                "A) Wick van Rossum",
+                "B) Rasmus Lerdorf",
+                "C) Guido van Rossum",
+                "D) Niene Stom"
+               ],
+      answer: "C) Guido van Rossum"
+    },
+/*Copy till here*/
+
+
+/* Q2 */     
+	{
+      numb: 2,
+      question: "Which type of Programming does Python support?",
+      options: [
+                "A) object-oriented programming ",
+                "B) structured programming",
+                "C) functional programming",
+                "D) all of the mentioned"
+               ],
+      answer: "D) all of the mentioned"
+    },
+/*Copy till here*/
+
+/* Q3 */     
+	{
+      numb: 3,
+      question: "Which of the following is the correct extension of the Python file?",
+      options: [
+                "A) .python",
+                "B) .pl",
+                "C) .py",
+                "D).p"
+               ],
+      answer: "C) .py"
+    },
+/*Copy till here*/
+	
+/* Q4 */     
+	{
+      numb: 4,
+      question: "Is Python code compiled or interpreted?",
+      options: [
+                "A) Python code is both compiled and interpreted",
+                "B) Python code is neither compiled nor interpreted",
+                "C) Python code is only compiled",
+                "D) Python code is only interpreted"
+               ],
+      answer: "B) Python code is neither compiled nor interpreted"
+    },
+/*Copy till here*/
+	
+/* Q5 */     
+	{
+      numb: 5,
+      question: "All keywords in Python are in _________",
+      options: [
+                "A) Capitalized",
+                "B) ower case",
+                "C) UPPER CASE",
+                "D) None of the mentioned"
+               ],
+      answer: "D) None of the mentioned"
+    },
+/*Copy till here*/
+
+/* Q6 */     
+	{
+      numb: 6,
+      question: "Which keyword is used for function in Python language?",
+      options: [
+                "A) Function",
+                "B) Def",
+                "C) Fun",
+                "D) Define"
+               ],
+      answer: "B) Def"
+    },
+/*Copy till here*/
+
+/* Q7 */     
+	{
+      numb: 7,
+      question: "Which of the following functions can help us to find the version of python that we are currently working on?",
+      options: [
+                "A) sys.version(1)",
+                "B) sys.version(0)",
+                "C) sys.version()",
+                "D) sys.version"
+               ],
+      answer: "A) sys.version(1)"
+    },
+/*Copy till here*/
+
+/* Q8 */ 
+	{
+      numb: 8,
+      question: "Python supports the creation of anonymous functions at runtime, using a construct called __________",
+      options: [
+                "A) pi",
+                "B) anonymous",
+                "C) lambda",
+                "D) none of the mentioned"
+               ],
+      answer: "C) lambda"
+    },
+/*Copy till here*/
+
+/* Q9 */ 
+	{
+      numb: 9,
+      question: "What does pip stand for python?",
+      options: [
+                "A) unlimited length",
+                "B) all private members must have leading and trailing underscores",
+                "C) Preferred Installer Program",
+                "D)  none of the mentioned"
+               ],
+      answer: "C) Preferred Installer Program"
+    },
+/*Copy till here*/
+
+
+/* Q10 */    
+	{
+      numb: 10,
+      question: "Which of the following functions is a built-in function in python?",
+      options: [
+                "A) factorial()",
+                "B) print()",
+                "C) seed()",
+                "D) sqrt()"
+               ],
+      answer: "B) print()"
+    },
+/*Copy till here*/
+
+
+]; /* Array close*/

6598ff2a6c139ab94c8fc855123bf43b737df41a
rspaul11 <rspaul1104@gmail.com>
2026-02-19 17:21:36 +0530

    Adding new file to the folder

---
+++ Paulora/src/main/webapp/JS/Quiz/ReasoningQuizQuestions.js
@@ -0,0 +1,150 @@
+
+/* Reasoning Quiz Questions
+ * creating an array and passing the number, questions, options, and answers
+ */
+
+let questions = [
+
+/* Q1 */
+    {
+      numb: 1,
+      question: "What number should come next <br> <strong style= 'color: violet'>7, 10, 8, 11, 9, 12, ...? </strong>",
+      options: [
+                "A) 7",
+                "B) 10",
+                "C) 12",
+                "D) 13"
+               ],
+      answer: "B) 10"
+    },
+
+/* Q2 */
+    {
+      numb: 2,
+      question: "What number should come next <br> <strong style='color: violet'>22, 21, 23, 22, 24, 23, ...? <strong>",
+      options: [
+                "A) 22",
+                "B) 24",
+                "C) 25",
+                "D) 26"
+               ],
+      answer: "C) 25"
+    },
+
+/* Q3 */    
+    {
+      numb: 3,
+      question: "Fill the blank in series <br> SCD, TEF, UGH, ____, WKL",
+      options: [
+                "A) CMN",
+                "B) UJI",
+                "C) VIJ",
+                "D) IJT"
+               ],
+      answer: "C) VIJ"
+    },
+  
+
+/* Q4 */
+  {
+      numb: 4,
+      question: "One morning Udai and Vishal were talking to each other face to face at a crossing. If Vishal's shadow was exactly to the left of Udai, which direction was Udai facing?",
+      options: [
+                "A) East",
+                "B) West",
+                "C) North",
+                "D) South"
+               ],
+      answer: "C) North"
+    },
+/*Copy till here*/
+  
+/* Q5 */     
+  {
+      numb: 5,
+      question: "If South-East becomes North, North-East becomes West and so on. What will West become?",
+      options: [
+                "A) North-East",
+                "B) North-West",
+                "C) South-East",
+                "D) South-West"
+               ],
+      answer: "C) South-East"
+    },
+/*Copy till here*/
+
+/* Q6 */     
+  {
+      numb: 6,
+      question: "A, P, R, X, S and Z are sitting in a row. S and Z are in the centre. A and P are at the ends. R is sitting to the left of A. Who is to the right of P?",
+      options: [
+                "A) A",
+                "B) X",
+                "C) S",
+                "D) Z"
+               ],
+      answer: "B) X"
+    },
+/*Copy till here*/
+
+/* Q7 */     
+  {
+      numb: 7,
+      question: "Pointing to a photograph of a boy Suresh said, 'He is the son of the only son of my mother.' How is Suresh related to that boy?",
+      options: [
+                "A) Brother",
+                "B) Uncle",
+                "C) Cousin",
+                "D) Father"
+               ],
+      answer: "D) Father"
+    },
+/*Copy till here*/
+
+/* Q8 */ 
+  {
+      numb: 8,
+      question: "Introducing a boy, a girl said, 'He is the son of the daughter of the father of my uncle.' How is the boy related to the girl?",
+      options: [
+                "A) Brother",
+                "B) Nephew",
+                "C) Uncle",
+                "D) Son-in-law"
+               ],
+      answer: "A) Brother"
+    },
+/*Copy till here*/
+
+/* Q9 */ 
+  {
+      numb: 9,
+      question: "Find out the alternative which will replace the question mark. <br> CUP : LIP :: BIRD : ?",
+      options: [
+                "A) BUSH",
+                "B) GRASS",
+                "C) FOREST",
+                "D) BEAK"
+               ],
+      answer: "D) BEAK"
+    },
+/*Copy till here*/
+
+
+/* Q10 */    
+  {
+      numb: 10,
+      question: "Find out the alternative which will replace the question mark. <br> Flow : River :: Stagnant : ?",
+      options: [
+                "A) Rain",
+                "B) Stream",
+                "C) Pool",
+                "D) Canal"
+               ],
+      answer: "C) Pool"
+    },
+/*Copy till here*/
+
+
+
+
+]; /* Array close*/
